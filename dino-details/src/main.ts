24776bc968c639c6c123061fd36e3498c672b2b1
gyorgydeakl <levente.gyorgydeak@credit4sales.hu>
2025-06-23 11:38:25 +0200

    add single-spa to dino-details

--- dino-details/src/main.ts
+++ dino-details/src/main.ts
@@ -1,11 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { NgZone } from '@angular/core';
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import {AppComponent} from './app/app.component';
 
 const lifecycles = singleSpaAngular({
   bootstrapFunction: props => {
-    return bootstrapApplication(App, {
+    return bootstrapApplication(AppComponent, {
       ...appConfig,
       providers: [
         ...getSingleSpaExtraProviders(),
