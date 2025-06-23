2473aeac9e8ad45a8b4ee9851bc55c81ae92b7ab
gyorgydeakl <levente.gyorgydeak@credit4sales.hu>
2025-06-23 11:02:29 +0200

    update navbar

--- navbar/src/root.component.tsx
+++ navbar/src/root.component.tsx
@@ -1,3 +1,24 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import React from "react";
+import "./root.component.css";
+
+export interface RootProps {
+  name?: string;
+}
+
+export default function Root({ name }: RootProps) {
+  return (
+    <nav className="navbar">
+      <a href="/" className="logo">
+        {name || "App"}
+      </a>
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/dino-list">Dinos</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
